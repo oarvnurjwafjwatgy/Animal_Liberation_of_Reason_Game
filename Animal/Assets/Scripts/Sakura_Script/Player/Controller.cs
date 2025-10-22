6da4ccadf1d89ee74ff33f1e0755929712ee451d
oarvnurjwafjwatgy <oibc34088@ojs.ac.jp>
2025-10-22 11:57:36 +0900

    10/22_AM_InputSystemの設定
    コントローラでもキーボードでもうごくようになりました。２～４になっても動くようにします。

--- Animal/Assets/Scripts/Sakura_Script/Player/Controller.cs
+++ Animal/Assets/Scripts/Sakura_Script/Player/Controller.cs
@@ -6,20 +6,43 @@ using UnityEngine.InputSystem;
 // Rigidbodyが必要であることを示す
 [RequireComponent(typeof(Rigidbody))]
 
-public class PlayerBase : MonoBehaviour
+public class Controller : MonoBehaviour
 {
+    public PlayerInput playerInput;
+
+    private Vector2 input_value;
+
+    private void Awake()
+    {
+        TryGetComponent(out playerInput);
+    }
 
 
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    { 
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        input_value = playerInput.actions["Move"].ReadValue<Vector2>();
+    }
+
+    //レフトスティックの傾きの取得
+    public Vector2 GetLeftStick()
+    {
+        return input_value;
     }
 }
--- Animal/Assets/Scripts/Sakura_Script/Player/PlayerManager.cs
+++ Animal/Assets/Scripts/Sakura_Script/Player/PlayerManager.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
+    //プレイヤーのプレファブを設定
+    private List<GameObject> PlayerPrefab = new List<GameObject>();
+    //プレイヤーの出現位置
+    private List<Transform> PlayerTransforms = new List<Transform>();
+    //生成されたプレイヤーを管理するリスト
+    // private List<Player>
+
+
     // Start is called before the first frame update
     void Start()
     {
