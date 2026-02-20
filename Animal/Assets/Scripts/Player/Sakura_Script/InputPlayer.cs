eee7a6074ac3e869b22b2120ce129d86928c3850
oarvnurjwafjwatgy <oibc34088@ojs.ac.jp>
2026-02-20 14:45:33 +0900

    0220_PM_エフェクトマネージャーの取得をコメントアウト

--- Animal/Assets/Scripts/Player/Sakura_Script/InputPlayer.cs
+++ Animal/Assets/Scripts/Player/Sakura_Script/InputPlayer.cs
@@ -53,8 +53,8 @@ public class InputPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        EffectManager = GameObject.Find("EffectManager");
-        Effect_Manager = EffectManager.GetComponent<EffectManager>();
+        //EffectManager = GameObject.Find("EffectManager");
+        //Effect_Manager = EffectManager.GetComponent<EffectManager>();
 
         // cameraObject と ghostObject は土台プレハブに元からあるはずなので取得
         // ただし、既に SetupDynamicReferences で設定されている場合は何もしない
