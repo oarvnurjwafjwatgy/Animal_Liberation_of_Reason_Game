39d6364f2e3956e31e18879cb5ffa0f822a0710a
KawaRyuu <167944356+KawaRyuu@users.noreply.github.com>
2026-02-23 00:05:28 +0900

    未完成：ステータスの受け渡しが上手くいってないため模索中
    「자새히는」
    ・UIと実際のデータが同期出来てなかったのでできるようにしました。
    ・攻撃の正しい数値が受け渡しされないため模索中

--- Animal/Assets/Scripts/Status/Kouki_files/PlayerDamage.cs
+++ Animal/Assets/Scripts/Status/Kouki_files/PlayerDamage.cs
@@ -20,7 +20,26 @@ public class PlayerDamage : MonoBehaviour
         
     }
 
-    public void SetDamage(int power)
+	// 攻撃判定（Collider等）に当たった瞬間の処理
+	void OnTriggerEnter(Collider other)
+	{
+		// 当たった相手のステータスを取得
+		Character_Status target = other.GetComponent<Character_Status>();
+
+		// 相手がステータスを持っていて、かつ自分自身でない場合
+		if (target != null && target != status)
+		{
+			// 1. 「自分の」理性解放が乗った今の攻撃力を取得
+			int myCurrentAtk = status.CurrentAttackPower;
+
+			// 2. 相手の TakeDamage に「自分の攻撃力」を直接叩き込む！
+			target.TakeDamage(myCurrentAtk);
+
+			Debug.Log($"<color=orange>【攻撃成功】{gameObject.name}が{other.name}に{myCurrentAtk}ダメ送信</color>");
+		}
+	}
+
+	public void SetDamage(int power)
     {
         Damages = (int)((float)power * (1f - (float)Defences / 100f));
     }
